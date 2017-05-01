d7de96476bfaa029654723dd9eaf447fde9e35bf
Troy Thompson <troycthompson@gmail.com>
2017-05-01 13:24:18 -0700

    Servers with dependencies

---
+++ single-server/main.tf
@@ -0,0 +1,40 @@
+# Configure a provider
+provider "aws" {
+  region = "us-west-2"
+}
+
+# Configure an AWS VPC
+resource "aws_vpc" "nonagon-vpc-1" {
+  cidr_block = "10.0.0.0/16"
+}
+
+# A subnet for the configured VPC
+resource "aws_subnet" "nonagon-subnet-1" {
+  vpc_id     = "${aws_vpc.nonagon-vpc-1.id}"
+  depends_on = ["aws_vpc.nonagon-vpc-1"]
+
+  cidr_block = "10.0.1.0/24"
+}
+
+# Configure an EC2 instance
+resource "aws_instance" "chp1-1" {
+  ami           = "ami-efd0428f"
+  instance_type = "t2.micro"
+  subnet_id     = "${aws_subnet.nonagon-subnet-1.id}"
+
+  tags {
+    Name = "Master server"
+  }
+}
+
+# Configure an EC2 instance with a dependency
+resource "aws_instance" "chp1-2-slave" {
+  ami           = "ami-efd0428f"
+  instance_type = "t2.micro"
+  subnet_id     = "${aws_subnet.nonagon-subnet-1.id}"
+  depends_on    = ["aws_instance.chp1-1"]
+
+  tags {
+    Name = "Slave Server"
+  }
+}
